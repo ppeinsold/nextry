935c38323e13384c1fd9071444ebf17957470ac4
ppeinsold <philip@herosphere.gg>
2020-09-25 19:18:31 +0200

    added current status

---
+++ precache-manifest.c06e69ef4364f4b311d15fc6f5838f59.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e963f1ba7fc313ca738",
+    "url": "/css/app.d5fa5dbf.css"
+  },
+  {
+    "revision": "cd0636bf548441999d1d",
+    "url": "/css/chunk-vendors.daab0ff0.css"
+  },
+  {
+    "revision": "fc9f3bce79dab3d84a6d",
+    "url": "/css/resume.2b99d838.css"
+  },
+  {
+    "revision": "0386678a1388efc248a7197de66e5904",
+    "url": "/img/profile.0386678a.jpg"
+  },
+  {
+    "revision": "33bf4cb51e99b665ab634091c0e26552",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e963f1ba7fc313ca738",
+    "url": "/js/app.21c82d56.js"
+  },
+  {
+    "revision": "cd0636bf548441999d1d",
+    "url": "/js/chunk-vendors.743a9443.js"
+  },
+  {
+    "revision": "fc9f3bce79dab3d84a6d",
+    "url": "/js/resume.c0cdb6fd.js"
+  },
+  {
+    "revision": "d49cf6b1baa26ed7d400b042944ed7d0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
